44236662c1582dc0113ed57b4a1e373ad3db50fb
Setrak Farahian <setoferahian@gmail.com>
2023-05-21 17:15:41 +0200

    Change level generator
    Changed name of level generator script

--- Assets/Scripts/LevelGeneration/EnemyGeneration.cs
+++ Assets/Scripts/LevelGeneration/EnemyGeneration.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelGeneration : MonoBehaviour
+public class EnemyGeneration : MonoBehaviour
 {
     public GameObject[] objects;
 
