d2b91f065252a17947ff047cf238623d548f52a7
Setrak Farahian <setoferahian@gmail.com>
2023-05-25 22:44:57 +0200

    Add LetterBox, Change Blob AI, Change Dungeon Tilemap
    Add LetterBox asset to adjust screen with resolution change,
    Change blob AI so it walks randomly arround the screen,
    Start to fix the size of the dungeons so it fits the screen well.

--- Assets/Scripts/Enemy/Blob.cs
+++ Assets/Scripts/Enemy/Blob.cs
@@ -4,10 +4,31 @@ using UnityEngine;
 
 public class Blob : Enemy
 {
+    private Vector2 randPos;
+    private float randTime;
+
+    protected override void Start() {
+        base.Start();
+        randTime = Random.Range(0f, 3f);
+        randPos = new Vector2(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3f));
+    }
+
+    protected override void ChasePlayer() {
+        if(Vector2.Distance(transform.position, randPos) > 0) {
+            transform.position = Vector2.MoveTowards(transform.position, randPos, speed * Time.deltaTime);
+        } else {
+            StartCoroutine(Wait());
+        }
+    }
     private void OnCollisionStay2D(Collision2D collision) {
         if (collision.collider.gameObject.CompareTag("Player") && Time.time > nextFire) {
             collision.gameObject.GetComponent<Health>().TakeDamage(1);
             nextFire = Time.time + fireRate;
         }
+    }
+
+    private IEnumerator Wait() {
+        yield return new WaitForSeconds(randTime);
+        randPos = new Vector2(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3f));
     }
 }
