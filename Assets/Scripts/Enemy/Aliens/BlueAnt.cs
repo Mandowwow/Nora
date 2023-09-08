5adfd78b81e204373b9c144f2ded636565132a92
Setrak Farahian <setoferahian@gmail.com>
2023-09-08 18:02:39 +0300

    Add blueantboss attack

---
+++ Assets/Scripts/Enemy/Aliens/BlueAnt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueAnt : Enemy
+{
+    [SerializeField] private GameObject spit = null;
+    [SerializeField] private GameObject barrel = null;
+    [SerializeField] private GameObject ball = null;
+    private Animator anim;
+
+    protected override void Start() {
+        base.Start();
+        anim = GetComponent<Animator>();
+        anim.Play("Spit");
+    }
+
+
+    public void Spit() {
+        //Projectile Attack
+        Instantiate(ball, barrel.transform.position, Quaternion.identity);
+    }
+}
