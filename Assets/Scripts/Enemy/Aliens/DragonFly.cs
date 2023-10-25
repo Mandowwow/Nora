12752d89b89aa9776ebb32164528d4fe62008366
Setrak Farahian <setoferahian@gmail.com>
2023-10-25 16:24:19 +0200

    Add DragonFly Enemy and DragonFly Level

--- Assets/Scripts/Enemy/Aliens/DragonFly.cs
+++ Assets/Scripts/Enemy/Aliens/DragonFly.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Blob : Enemy
+public class DragonFly : Enemy
 {
     private Vector2 randPos;
     private float randTime;
@@ -12,13 +12,15 @@ public class Blob : Enemy
     protected override void Start() {
         base.Start();
         randTime = Random.Range(1f, 3f);
-        randPos = new Vector2(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3f));
+        randPos = new Vector2(Random.Range(-7.4f, 7.4f), Random.Range(-3.5f, 3.5f));
     }
     protected override void ChasePlayer() {
         Debug.Log(Vector2.Distance(transform.position, randPos));
         if (Vector2.Distance(transform.position, randPos) > 0.1) {
             transform.position = Vector2.MoveTowards(transform.position, randPos, speed * Time.deltaTime);
-        } else if (Vector2.Distance(transform.position, randPos) <= 0.1 && canMove == true) {    
+            TurnDirection();
+        }
+        else if (Vector2.Distance(transform.position, randPos) <= 0.1 && canMove == true) {
             StartCoroutine(Wait());
         }
     }
@@ -30,7 +32,7 @@ public class Blob : Enemy
     }
     protected override void Dying() {
         base.Dying();
-        if(health <= 0) {
+        if (health <= 0) {
             BlowUp();
         }
     }
@@ -43,5 +45,13 @@ public class Blob : Enemy
         randPos = new Vector2(Random.Range(-6.4f, 6.4f), Random.Range(-3.5f, 3f));
         randTime = Random.Range(1f, 3f);
         canMove = true;
+    }
+    private void TurnDirection() {
+        if (transform.position.x > randPos.x) {
+            sprite.flipX = true;
+        }
+        else {
+            sprite.flipX = false;
+        }
     }
 }
