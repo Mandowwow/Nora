8c4224cec57696270c66decd97f17322b4bf3497
Setrak Farahian <setoferahian@gmail.com>
2023-11-08 17:10:15 +0100

    Add HealthIncrease
    change some ui stuff and added health increase

---
+++ Assets/Scripts/PickUps/HealthIncrease.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthIncrease : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            IncreaseHealth();
+            Destroy(this.gameObject);
+        }
+    }
+    private void IncreaseHealth() {
+        CharacterStats.NumOfHearts += 1;
+        CharacterStats.Health += 1;
+    }
+}
--- Assets/Scripts/PickUps/HealthPickUp.cs
+++ Assets/Scripts/PickUps/HealthPickUp.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public class HealthPickUp : MonoBehaviour
 {
-    private CharacterStats stats;
-
-    private void Start() {
-        stats = GameObject.FindGameObjectWithTag("Manager").GetComponent<CharacterStats>();
-    }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.CompareTag("Player")) {
             if(CharacterStats.Health == CharacterStats.NumOfHearts) {
