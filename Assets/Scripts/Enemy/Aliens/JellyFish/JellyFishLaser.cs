0d52d28bf38766d9fa946e0837aa9025441e2462
Setrak Farahian <setoferahian@gmail.com>
2023-11-15 19:35:02 +0100

    Add JellyFish mob

---
+++ Assets/Scripts/Enemy/Aliens/JellyFish/JellyFishLaser.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyFishLaser : MonoBehaviour
+{
+    private Transform player;
+    private bool canLook = true;
+    private Vector2 direction;
+    [SerializeField] private GameObject laserPrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        Invoke("StopLooking", 2f);
+        Invoke("Shoot", 3f);
+        Destroy(this.gameObject, 3.01f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canLook) {
+            CalcDirection();
+        }       
+    }
+
+    private void CalcDirection() {
+        Vector3 playerPos = player.transform.position;
+
+        direction = new Vector2(
+        playerPos.x - transform.position.x,
+        playerPos.y - transform.position.y);
+        transform.right = direction;
+    }
+
+    private void StopLooking() {
+        canLook = false;
+    }
+
+    private void Shoot() {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.transform.up = direction;
+        laser.GetComponent<Rigidbody2D>().velocity = direction.normalized * 40f;
+    }
+}
---
+++ Assets/Scripts/Enemy/Aliens/JellyFish/JellyFishPurple.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyFishPurple : Enemy
+{
+    private bool canChase = false;
+    protected override void Start() {
+        Invoke("Shoot", 3f);
+        base.Start();
+        speed = Random.Range(1f, 1.5f);
+    }
+    protected override void ChasePlayer() {
+        if (canChase) {
+            base.ChasePlayer();
+        }
+        TurnDirection();       
+    }
+    private void RunAway() {
+        if (Vector2.Distance(transform.position, player.position) < 5) {
+            TurnDirection();
+            Vector3 targetPos = player.position;
+            Vector2 direction = new Vector2(
+                targetPos.x - transform.position.x,
+                targetPos.y - transform.position.y);
+            //direction = Quaternion.AngleAxis(45, Vector2.up) * direction;
+            rb.MovePosition(rb.position + -direction.normalized * speed * Time.fixedDeltaTime);
+        }
+    }
+    private void TurnDirection() {
+        if (player != null) {
+            if (transform.position.x > player.transform.position.x) {
+                sprite.flipX = true;               
+            }
+            else {
+                sprite.flipX = false;
+            }
+        }
+    }
+    private void Shoot() {
+        
+        canChase = true;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Controller") && Time.time > nextFire) {
+            collision.gameObject.GetComponent<Health>().TakeDamage(1);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
