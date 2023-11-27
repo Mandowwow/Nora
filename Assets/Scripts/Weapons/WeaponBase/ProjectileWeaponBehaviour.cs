693884296b125a606ff64a9e35586a24ada45bf2
Setrak Farahian <setoferahian@gmail.com>
2023-11-27 17:36:28 +0100

    new player bullet behaviour
    normal shooting is now part of new weapon system

--- Assets/Scripts/Weapons/WeaponBase/ProjectileWeaponBehaviour.cs
+++ Assets/Scripts/Weapons/WeaponBase/ProjectileWeaponBehaviour.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Base script for all projectiles behaviour scripts to inherit
+/// </summary>
 public class ProjectileWeaponBehaviour : MonoBehaviour
 {
+    protected Animator anim;
+    protected Rigidbody2D rb;
     protected Vector3 direction;
     public float destroyAfterSeconds;
 
     protected virtual void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         Destroy(gameObject, destroyAfterSeconds);
     }
 
---
+++ Assets/Scripts/Weapons/WeaponBehaviours/BulletBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : ProjectileWeaponBehaviour
+{
+    BulletController bc;
+
+    protected override void Start() {
+        base.Start();
+        bc = FindObjectOfType<BulletController>();
+        rb.velocity = direction * bc.speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Enemy")) {
+            collision.GetComponent<Enemy>().TakeDamage(1);
+            GetComponent<Collider2D>().enabled = false;
+            gameObject.transform.localScale += new Vector3(0.2f, 0.2f);
+            anim.Play("Impact");
+            rb.velocity = new Vector2(0, 0);
+            Destroy(this.gameObject, 0.3f);
+        }
+        else if (collision.CompareTag("Wall")) {
+            Destroy(this.gameObject);
+        }
+    }
+}
