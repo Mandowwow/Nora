21f2c60516dfcb24f6eb6ff55c77b39cda6b5fca
Setrak Farahian <setoferahian@gmail.com>
2024-01-14 20:12:53 +0200

    Add laser to new weapon system

--- Assets/Scripts/Weapons/WeaponControllers/LaserController.cs
+++ Assets/Scripts/Weapons/WeaponControllers/LaserController.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class LaserController : WeaponsController
 {
+    private SpriteRenderer render;
     protected override void Start() {
         base.Start();
+        GameObject player = GameObject.FindWithTag("Controller");
+        render = player.GetComponent<SpriteRenderer>();
     }
 
     protected override void Attack() {
         if (Input.GetKey("right")) {
-            Shoot(new Vector3(1f, 0f, 0f));
+            Shoot(new Vector3(0f, 0f, 0f));
         }
         else if (Input.GetKey("left")) {
-            Shoot(new Vector3(-1f, 0f, 0f));
+            Shoot(new Vector3(0f, 0f, 180f));
         }
         else if (Input.GetKey("up")) {
-            Shoot(new Vector3(0f, 1f, 0f));
+            Shoot(new Vector3(0f, 0f, 90f));
         }
         else if (Input.GetKey("down")) {
-            Shoot(new Vector3(0f, -1f, 0f));
+            Shoot(new Vector3(0f, 0f, -90f));
 
         }
     }
@@ -28,8 +31,15 @@ public class LaserController : WeaponsController
         base.Attack();
         GameObject spawnedBullet = Instantiate(weaponData.Prefab);
         spawnedBullet.transform.position = transform.position;
-        spawnedBullet.GetComponent<LaserBehaviour>().DirectionCalc(dir);
-        //spawnedBullet.transform.rotation = Quaternion.Euler(0,90,0);
-        Debug.Log("Here");
+        //spawnedBullet.GetComponent<LaserBehaviour>().DirectionCalc(dir);
+        spawnedBullet.transform.rotation = Quaternion.Euler(dir);
+        spawnedBullet.transform.parent = transform;
+        StartCoroutine(RevertLayer());
+    }
+
+    private IEnumerator RevertLayer() {
+        render.sortingOrder = -1;
+        yield return new WaitForSeconds(0.5f);
+        render.sortingOrder = 20;
     }
 }
