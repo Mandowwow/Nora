93bec051d65fce154e373cad2cc2a77e4d28b6e0
Setrak Farahian <setoferahian@gmail.com>
2024-01-17 17:49:11 +0200

    Testing out UI

---
+++ Assets/Scripts/LevelUp/ButtonInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ButtonInfo", menuName = "ScriptableObjects/ButtonInfo")]
+public class ButtonInfo : ScriptableObject
+{
+    [SerializeField] private string _buttonName;
+    [SerializeField] private string _buttonDescription;
+    [SerializeField] private Sprite _buttonImage;
+
+    public string ButtonName
+    {
+        get => _buttonName;
+        set => _buttonName = value;
+    }
+
+    public string ButtonDescription
+    {
+        get => _buttonDescription;
+        set => _buttonDescription = value;
+    }
+
+    public Sprite ButtonImage
+    {
+        get => _buttonImage;
+        set => _buttonImage = value;
+    }
+
+    public void OnButtonClick() {
+        Debug.Log(ButtonName);
+    }
+
+
+    
+}
---
+++ Assets/Scripts/LevelUp/ButtonManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ButtonManager : MonoBehaviour
+{
+    public List<ButtonInfo> buttoninfoList;
+    Button button;
+
+    private static HashSet<int> assignedIndices = new HashSet<int>();
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+
+        ButtonInfo randomButtonInfo = GetRandomButtonInfo();
+
+        SetupButton(button, randomButtonInfo);
+    }
+
+    ButtonInfo GetRandomButtonInfo() {
+        if (buttoninfoList.Count > 0) {
+            // Keep trying to get a random index until an unassigned one is found
+            int randomIndex;
+            do {
+                randomIndex = Random.Range(0, buttoninfoList.Count);
+            } while (assignedIndices.Contains(randomIndex));
+
+            // Add the index to the set of assigned indices
+            assignedIndices.Add(randomIndex);
+
+            return buttoninfoList[randomIndex];
+        }
+        else {
+            Debug.LogError("No ButtonInfo objects in the list.");
+            return null;
+        }
+    }
+
+    public void SetupButton(Button button, ButtonInfo buttonInfo) {
+        //buttonInfo.OnButtonClick();
+        var textComponents = button.GetComponentsInChildren<TextMeshProUGUI>();
+        button.image.sprite = buttonInfo.ButtonImage;
+        textComponents[0].text = buttonInfo.ButtonName;
+        textComponents[1].text = buttonInfo.ButtonDescription;
+        button.onClick.AddListener(() => buttonInfo.OnButtonClick());
+    }
+}
