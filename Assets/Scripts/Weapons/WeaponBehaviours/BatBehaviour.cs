b02ca6c399526ceb4e15b189f8d30ad14e6109b5
Setrak Farahian <setoferahian@gmail.com>
2024-03-19 15:11:46 +0100

    Add missile animation
    Add BatBehaviour.cs to play impact animation of missile on hit

--- Assets/Scripts/Weapons/WeaponBehaviours/BatBehaviour.cs
+++ Assets/Scripts/Weapons/WeaponBehaviours/BatBehaviour.cs
@@ -11,4 +11,27 @@ public class BatBehaviour : ProjectileWeaponBehaviour
         bc.FindClosestEnemy(this.GetComponent<Rigidbody2D>(), transform);
     }
 
+    protected override void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Enemy")) {
+            collision.GetComponent<Enemy>().TakeDamage(currentDamage);
+            Impact();
+        }
+        else if (collision.CompareTag("Wall")) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void Impact() {
+        currentPierce--;
+        if (currentPierce <= 0) {
+            GetComponent<Collider2D>().enabled = false;
+            gameObject.transform.localScale -= new Vector3(0.5f, 0.5f);
+            gameObject.transform.localRotation = Quaternion.identity;
+            anim.Play("Impact");
+            rb.velocity = new Vector2(0, 0);
+            Destroy(this.gameObject, 0.3f);
+
+        }
+    }
+
 }
