09d21e1fcb898bc3bd231c7d0562959873bc89d8
Setrak Farahian <setoferahian@gmail.com>
2024-03-21 19:49:18 +0100

    Add  SawBlade weapon
    add sawblade.scriptableobject
    add sawbladebehaviour.cs
    add sawbladecontroller.cs
    add sawblade.prefab

---
+++ Assets/Scripts/Weapons/WeaponBehaviours/SawBladeBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBladeBehaviour : ProjectileWeaponBehaviour
+{
+    Vector3 lastVelocity;
+    protected override void Start() {
+        base.Start();
+        rb.velocity = direction * currentSpeed;
+    }
+
+    private void Update() {
+        transform.Rotate(new Vector3(0f, 0f, 396f) * Time.deltaTime);
+    }
+    private void FixedUpdate() {
+        lastVelocity = rb.velocity;
+    }
+    protected override void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Enemy")) {
+            collision.GetComponent<Enemy>().TakeDamage(currentDamage);
+            ReducePierceBullet();
+        }
+        else if (collision.CompareTag("Wall")) {
+            Vector2 collisionNormal = collision.ClosestPoint(transform.position) - (Vector2)transform.position;
+            float speed = lastVelocity.magnitude;
+            collisionNormal.Normalize();
+            Vector3 direction = Vector3.Reflect(lastVelocity.normalized, collisionNormal);
+            rb.velocity = direction * Mathf.Max(speed, 0f);
+        }
+    }
+
+    public void ReducePierceBullet() {
+        currentPierce--;
+        if (currentPierce <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/WeaponControllers/SawBladeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBladeController : WeaponsController
+{
+    // Start is called before the first frame update
+    protected override void Start() {
+        base.Start();
+    }
+
+    protected override void Attack() {
+        base.Attack();
+        int direction = Random.Range(0, 4);
+        switch (direction) {
+            case 0:
+                Shoot(Quaternion.Euler(0, 0, 45) * Vector3.down);
+                break;
+            case 1:
+                Shoot(Quaternion.Euler(0, 0, -45) * Vector3.up);
+                break;
+            case 2:
+                Shoot(Quaternion.Euler(0, 0, 45) * Vector3.up);
+                break;
+            case 3:
+                Shoot(Quaternion.Euler(0, 0, -45) * Vector3.up);
+                break;
+        }
+
+    }
+
+    void Shoot(Vector3 dir) {
+        GameObject spawnedBlade = Instantiate(weaponData.Prefab);
+        spawnedBlade.transform.position = transform.position;
+        spawnedBlade.GetComponent<SawBladeBehaviour>().DirectionCalc(dir);
+    }
+}
