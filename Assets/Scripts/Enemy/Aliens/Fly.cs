40f48ad3c32c13f32aa5efe9368c4a528a7da46e
Setrak Farahian <setoferahian@gmail.com>
2024-03-22 15:17:43 +0100

    Add Fly/ new blob
    Add Fly.cs
    Add neew blob sprites

---
+++ Assets/Scripts/Enemy/Aliens/Fly.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly : Enemy
+{
+    protected override void ChasePlayer() {
+        base.ChasePlayer();
+        TurnDirection();
+    }
+
+    private void TurnDirection() {
+        if (player != null) {
+            if (transform.position.x > player.transform.position.x) {
+                sprite.flipX = true;
+            }
+            else {
+                sprite.flipX = false;
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Controller") && Time.time > nextFire) {
+            collision.gameObject.GetComponent<Health>().TakeDamage(1);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
