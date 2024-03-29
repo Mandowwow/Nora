d688bbe7f085b19c8b8c757b67df9f0c121dce72
Setrak Farahian <setoferahian@gmail.com>
2024-03-29 12:25:06 +0100

    Add CrystalPurpleCreature.prefab
    Add CrystyalPurple.cs
    Add CrystalPurple.animation
    Add CrystalPurple_ball.cs
    Add Level24.prefab
    Update Level23.prefab (missing transition canvas)

---
+++ Assets/Scripts/Enemy/Crystal/Purple/CrystalPurple.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalPurple : Enemy
+{
+    [SerializeField]
+    GameObject prefab = null;
+    protected override void ChasePlayer() {
+        base.ChasePlayer();
+        TurnDirection();
+    }
+    protected override void Dying() {
+        if (health <= 0) {
+            BlowUp();
+            Destroy(this.gameObject);
+            FindEnemies.Enemies.Remove(this.gameObject);
+            if (FindEnemies.Enemies.Count <= 0) {
+                Instantiate(findEnemies.Portal, new Vector3(0, 0, 0), Quaternion.identity);
+                GameManager.instance.StartLevelUp();
+            }
+        }
+    }
+    private void BlowUp() {
+        GameObject spawnedBullet = Instantiate(prefab);
+        spawnedBullet.transform.position = transform.position;
+    }
+    private void TurnDirection() {
+        if (player != null) {
+            if (transform.position.x > player.transform.position.x) {
+                sprite.flipX = false;
+            }
+            else {
+                sprite.flipX = true;
+            }
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Controller") && Time.time > nextFire) {
+            collision.gameObject.GetComponent<Health>().TakeDamage(1);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
