c91bd4c8f747a19f8887c456cea3c315e2997dd6
Setrak Farahian <setoferahian@gmail.com>
2024-03-30 00:52:43 +0100

    Add JellyFishPurpleBoss
    Add JellyFishPurpleBoss.anim
    Add JellyFishPurpleBoss.prefab
    Add JellyFishPurpleBoss.cs

---
+++ Assets/Scripts/Enemy/Aliens/JellyFish/JellyFishPurpleBoss/JellyFishPurpleBoss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyFishPurpleBoss : Enemy
+{
+    protected override void ChasePlayer() {
+        base.ChasePlayer();
+        TurnDirection();
+    }
+
+    private void TurnDirection() {
+        if (player != null) {
+            if (transform.position.x > player.transform.position.x) {
+                sprite.flipX = true;
+            }
+            else {
+                sprite.flipX = false;
+            }
+        }
+    }
+}
