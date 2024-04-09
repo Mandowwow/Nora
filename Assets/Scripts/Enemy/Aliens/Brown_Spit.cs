97b24d448e38ea1f6127d3a04ee4563451e19e06
Setrak Farahian <setoferahian@gmail.com>
2024-04-09 19:22:33 +0200

    Add BownSpit Enemy
    started implementation of brown spit enemy (need to work on what he spits out)

---
+++ Assets/Scripts/Enemy/Aliens/Brown_Spit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brown_Spit : Enemy
+{
+    [SerializeField] private GameObject spit = null;
+    [SerializeField] private GameObject barrel = null;
+    private Animator anim;
+    protected override void Start() {
+        base.Start();
+        anim = GetComponent<Animator>();
+    }
+
+    protected override void ChasePlayer() {
+        if (player != null) {
+            if (Vector2.Distance(transform.position, player.position) >= 0f) {
+                anim.Play("Spit");
+            }
+        }
+    }
+
+    public void Spit() {
+        if (Vector2.Distance(transform.position, player.position) < 13f) {
+            Instantiate(spit, barrel.transform.position, Quaternion.identity);
+            GameObject spawnedPrefab = Instantiate(spit);
+            spawnedPrefab.transform.position = barrel.transform.position;
+
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Controller") && Time.time > nextFire) {
+            collision.gameObject.GetComponent<Health>().TakeDamage(1);
+            nextFire = Time.time + fireRate;
+        }
+    }
+}
