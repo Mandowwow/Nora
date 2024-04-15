4db15691d3664d848417914eef7f6422dd98dd05
Setrak Farahian <setoferahian@gmail.com>
2024-04-15 18:25:58 +0200

    Add new Level Generate System.
    This new system will be using Scriptable Objects to store the data for levels, and create sets of levels to generate from.
    
    Add LevelData.cs
    Add LevelSet.cs
    Add LevelManager.cs
    
    Add folders for scriptabelObjects.

---
+++ Assets/Scripts/LevelGeneration/LevelData.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "Level/LevelData")]
+public class LevelData : ScriptableObject
+{
+    [SerializeField] GameObject _levelPrefab;
+    [SerializeField] LevelPhase _phase;
+
+    public enum LevelPhase {
+        First,
+        Second,
+        Third,
+        Fourth,
+        Boss
+    }
+
+    public GameObject Prefab
+    {
+        get => _levelPrefab;
+        private set => _levelPrefab = value;
+    }
+
+    public LevelPhase Phase
+    {
+        get => _phase;
+        private set => _phase = value;
+    }
+}
