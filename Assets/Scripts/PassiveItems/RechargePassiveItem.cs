b1ad79924b53215da31d382a3b950df9e0d65e56
Setrak Farahian <setoferahian@gmail.com>
2024-04-22 17:32:27 +0200

    Update RechargePassiveItem.cs
    Change recharge to give 3 health instead

--- Assets/Scripts/PassiveItems/RechargePassiveItem.cs
+++ Assets/Scripts/PassiveItems/RechargePassiveItem.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class RechargePassiveItem : PassiveItem
 {
     protected override void ApplyModifier() {
-        while(PlayerStats.CurrentHealth != ps.CurrentNumOfHearts) {
-            PlayerStats.CurrentHealth += 1;
+        //while(PlayerStats.CurrentHealth != ps.CurrentNumOfHearts) {
+        //    PlayerStats.CurrentHealth += 1;
+        //}
+
+        for (int i = 0; i < 3; i++) {
+            if(PlayerStats.CurrentHealth != ps.CurrentNumOfHearts) {
+                PlayerStats.CurrentHealth += 1;
+            }
         }
     }
 }
