d6cb272845b03e69309732e67398be8ff8879ef8
Setrak Farahian <setoferahian@gmail.com>
2024-04-27 14:40:42 +0200

    Add Boss mechanics
    Add Star.cs
    Add StarController.cs
    Add prefab for boss

---
+++ Assets/Scripts/Enemy/ButterflyBlue/Star.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+
+    public Animator anim;
+    public float speed;
+    private GameObject target;
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot() {
+        Vector3 targetPos = target.transform.position;
+        Vector2 direction = new Vector2(
+            targetPos.x - transform.position.x,
+            targetPos.y - transform.position.y);
+        //transform.up = direction;
+        rb.velocity = direction.normalized * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.CompareTag("Wall")) {
+            //Destroy(this.gameObject);
+        }
+        else if (collision.CompareTag("Player")) {
+            collision.gameObject.GetComponentInParent<Health>().TakeDamage(1);
+            //Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy/ButterflyBlue/StarController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour {
+
+    public GameObject[] stars;
+    private int index = 0;
+    // Start is called before the first frame update
+    void Start() {
+        ShuffleArray(stars);
+        InvokeRepeating("StarTransform", 3f, 5f);
+    }
+
+    private void StarTransform() {
+        stars[index].GetComponent<Star>().anim.Play("Transform");
+        StartCoroutine(Wait()); 
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(1f);
+        stars[index].GetComponent<Star>().speed = Random.Range(1.5f, 2.25f);
+        if (index <= stars.Length) {
+            index++;
+        }
+    }
+
+    void ShuffleArray(GameObject[] array) {
+        System.Random rng = new System.Random();
+        int n = array.Length;
+        while (n > 1) {
+            n--;
+            int k = rng.Next(n + 1);
+            GameObject temp = array[k];
+            array[k] = array[n];
+            array[n] = temp;
+        }
+    }
+}
