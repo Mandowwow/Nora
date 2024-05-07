8efb07934cfd8fa6e1f176915c1bea474a3cb74e
Setrak Farahian <setoferahian@gmail.com>
2024-05-07 16:53:35 +0200

    Update Robot boss and add Portal
    1)Robot_One.prefab
    -Robot one boss now instantiates a portal that instantiates the drone orbs.
    
    2)DroneSummon.cs
    -function that instantiates the drone orbs is triggered at the end of summon animation.
    
    Art assets for the drone summon portal have been added, including sprites and animations.

---
+++ Assets/Scripts/Enemy/Robots/Robot_One/DroneSummon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneSummon : MonoBehaviour
+{
+    [SerializeField] GameObject dronePrefab;
+    public void InstantiateDrones() {
+        GameObject spawnedDrone = Instantiate(dronePrefab);
+        spawnedDrone.transform.position = transform.position;
+        Destroy(this.gameObject);
+    }
+}
