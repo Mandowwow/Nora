3a63c6bd4b21f7ccff9d28967d80467f616d83c8
Setrak Farahian <setoferahian@gmail.com>
2024-05-12 22:11:55 +0200

    Add Robot Two Boss enemy
    1) Robot_Two.cs
    
    -Initialize enemy behavior script.
    -SawBlades()  function to instantiate individual saw blades with specified random directions
    
    2)Robot_Two_SawBladeBehaviour.cs
    
    -Initialize sawblade prefab behaviour.
    -has same functionality has player sawblade weapon.
    
    Art assets for the boss and its sawblades have been added, including sprites, animations and prefabs.

---
+++ Assets/Scripts/Enemy/Robots/Robot_Two/Robot_Two.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot_Two : Enemy
+{
+    [SerializeField] GameObject sawBladePrefab;
+    int[] angles = new int[2] { 45, -45 };
+    int[] directions = new int[2] { -1, 1 };
+    int randomAngleValue;
+    int randomDirValue;
+
+    protected override void Start() {
+        base.Start();
+        InvokeRepeating("SpawnSawBlade", 5f, 8f);
+    }
+
+    void SpawnSawBlade() {
+        randomAngleValue = Random.Range(0, 2);
+        randomDirValue = Random.Range(0, 2);
+        SawBlades(Quaternion.Euler(0, 0, angles[randomAngleValue]) * new Vector3(0, directions[randomDirValue], 0));
+    }
+
+    void SawBlades(Vector3 dir) {
+        GameObject spanwedPrefab = Instantiate(sawBladePrefab);
+        spanwedPrefab.transform.position = transform.position;
+        spanwedPrefab.GetComponent<Robot_Two_SawBladeBehaviour>().DirectionCalc(dir);
+    }
+}
---
+++ Assets/Scripts/Enemy/Robots/Robot_Two/Robot_Two_SawBladeBehaviour.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot_Two_SawBladeBehaviour : MonoBehaviour
+{
+    [SerializeField]private float speed = 8f;
+    private Vector3 direction;
+    private Rigidbody2D rb;
+    public float destroyAfterSeconds;
+    Vector3 lastVelocity;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = direction * speed;
+        Destroy(gameObject, destroyAfterSeconds);
+    }
+
+    private void Update() {
+        transform.Rotate(new Vector3(0f, 0f, 396f) * Time.deltaTime);
+    }
+    private void FixedUpdate() {
+        lastVelocity = rb.velocity;
+    }
+
+    public void DirectionCalc(Vector3 dir) {
+        direction = dir;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            collision.gameObject.GetComponentInParent<Health>().TakeDamage(1);
+            Destroy(this.gameObject);
+        }
+        else if (collision.CompareTag("Wall")) {
+            Vector2 collisionNormal = collision.ClosestPoint(transform.position) - (Vector2)transform.position;
+            float speed = lastVelocity.magnitude;
+            collisionNormal.Normalize();
+            Vector3 direction = Vector3.Reflect(lastVelocity.normalized, collisionNormal);
+            rb.velocity = direction * Mathf.Max(speed, 0f);
+        }
+    }
+
+}
